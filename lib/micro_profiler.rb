5b6617d0eefc393a087c2f47383e12851d023aaf
Jayson Virissimo <jayson.virissimo@asu.edu>
2017-11-14 13:20:09 -0700

    Allow toggling garbage collection

--- lib/micro_profiler.rb
+++ lib/micro_profiler.rb
@@ -1,11 +1,13 @@
 require 'micro_profiler/version'
 
 class MicroProfiler
-  def self.measure(block: Proc.new)
-    new.measure(block)
+  def self.measure(garbage_collection: true, block: Proc.new)
+    new(garbage_collection: garbage_collection).measure(block)
   end
 
-  def initialize; end
+  def initialize(garbage_collection: true)
+    @garbage_collection = garbage_collection
+  end
 
   def measure(block)
     starting_memory_usage
@@ -19,6 +21,8 @@ class MicroProfiler
 
   private
 
+  attr_reader :garbage_collection
+
   def current_memory_usage
     `ps -o rss= -p #{Process.pid}`.to_i / 1024
   end
@@ -29,6 +33,14 @@ class MicroProfiler
 
   def formatted_memory_usage
     format('%d MB', (ending_memory_usage - starting_memory_usage))
+  end
+
+  def handle_garbage_collection
+    if garbage_collection
+      GC.start
+    else
+      GC.disable
+    end
   end
 
   def print_results
--- spec/micro_profiler_spec.rb
+++ spec/micro_profiler_spec.rb
@@ -1,20 +1,25 @@
 require 'spec_helper'
 
 RSpec.describe MicroProfiler do
+  subject do
+    MicroProfiler.measure do
+      100.times { 'ABC' * 100 }
+    end
+  end
+
   it 'has a version number' do
     expect(MicroProfiler::VERSION).not_to be nil
   end
 
+  it 'defaults to using garbage collection' do
+    expect(MicroProfiler.new.send(:garbage_collection)).to be(true)
+  end
+
   describe 'prints memory usage in megabytes' do
     let(:expected_message) { 'Memory Usage: ' }
-    subject do
-      MicroProfiler.measure do
-        100.times { 'ABC' * 100 }
-      end
-    end
 
     it { expect { subject }.to output(/#{expected_message}/).to_stdout }
   end
 
-  xdescribe 'returns the yielded result'
+  xdescribe 'returns the yielded result' 
 end
