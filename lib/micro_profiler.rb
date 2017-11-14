e168935db4a8178525ab79a764160d3522fdd4dd
Jayson Virissimo <jayson.virissimo@asu.edu>
2017-11-14 12:36:27 -0700

    Print memory usage

--- lib/micro_profiler.rb
+++ lib/micro_profiler.rb
@@ -1,6 +1,41 @@
-require "micro_profiler/version"
+require 'micro_profiler/version'
 
 class MicroProfiler
-  def initialize
+  def self.measure(block: Proc.new)
+    new.measure(block)
+  end
+
+  def initialize; end
+
+  def measure(block)
+    starting_memory_usage
+
+    block.call
+
+    ending_memory_usage
+
+    print_results
+  end
+
+  private
+
+  def current_memory_usage
+    `ps -o rss= -p #{Process.pid}`.to_i / 1024
+  end
+
+  def ending_memory_usage
+    @ending_memory_usage ||= current_memory_usage
+  end
+
+  def formatted_memory_usage
+    format('%d MB', (ending_memory_usage - starting_memory_usage))
+  end
+
+  def print_results
+    puts "Memory Usage: #{formatted_memory_usage}"
+  end
+
+  def starting_memory_usage
+    @starting_memory_usage ||= current_memory_usage
   end
 end
--- spec/micro_profiler_spec.rb
+++ spec/micro_profiler_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 RSpec.describe MicroProfiler do
-  it "has a version number" do
+  it 'has a version number' do
     expect(MicroProfiler::VERSION).not_to be nil
   end
 
-  xdescribe "prints memory usage in megabytes"
+  describe 'prints memory usage in megabytes' do
+    let(:expected_message) { 'Memory Usage: ' }
+    subject do
+      MicroProfiler.measure do
+        100.times { 'ABC' * 100 }
+      end
+    end
 
-  xdescribe "returns the yielded result"
+    it { expect { subject }.to output(/#{expected_message}/).to_stdout }
+  end
+
+  xdescribe 'returns the yielded result'
 end
